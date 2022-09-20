3ab1cb94d64c58773937d047a62f7594ace2a059
hatdeck <daniel.bryce.hall@gmail.com>
2022-09-20 12:56:42 -0700

    refactor and debug binary search

--- BinarySearchList.ts
+++ BinarySearchList.ts
@@ -2,13 +2,15 @@ export default function bs_list(haystack: number[], needle: number): boolean {
         let lo = 0
         let hi = haystack.length
         // binary search uses log2(length) iterations
-        // TODO: verify that you don't need to floor
-        // recognize that the log can take noninteger values
-        const iters = Math.log2(haystack.length)
+        // worst case, you need to do up to the log of the length + 1
+        // you don't need to truncate this because it is an inequality
+        const iters = Math.log2(haystack.length) + 1
         for (let i = 0; i < iters; i++){
           // midpoint of available search space
           const m = Math.floor(lo + (hi - lo)/2)
           // value to check
+          // because of 0 indexing, this will be the midpoint for odd lengths
+          // one after the midpoint for even lengths
           const v = haystack[m]
           // if the value is the one you're looking for, you're done
           if (v == needle){
@@ -16,24 +18,16 @@ export default function bs_list(haystack: number[], needle: number): boolean {
           } else if (v > needle){
             // if the value is larger, you need to check the search space
             // below it
-            hi = m
+            // you already checked haystack[m]. Your search space should be
+            // precisely that area that could contain the value and hasn't yet
+            // been checked.
+            hi = m - 1
           } else {
             // the other option is that the value is smaller, and you need
             // to search the space above it
-            low = m
+            lo = m + 1
           }
         }
-//        do {
-//            const m = Math.floor(lo + (hi - lo)/ 2)
-//            const v = haystack[m]
-//            if (v === needle){
-//                return true
-//            } else if (v > needle) {
-//                hi = m
-//            } else {
-//                lo = m + 1
-//            }
-//        } while (lo < hi)
 
         return false
 }

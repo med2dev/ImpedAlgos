ae7a7eeaaa8de2ef2cad28a9946be877d89f9871
hatdeck <daniel.bryce.hall@gmail.com>
2022-09-20 14:35:59 -0700

    debug

--- BinarySearchList.ts
+++ BinarySearchList.ts
@@ -1,6 +1,8 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+        // set lo to the index position of the first value
         let lo = 0
-        let hi = haystack.length
+        // hi gets the index position of the last value
+        let hi = haystack.length - 1
         // binary search uses log2(length) iterations
         // worst case, you need to do up to the log of the length + 1
         // you don't need to truncate this because it is an inequality
@@ -10,7 +12,7 @@ export default function bs_list(haystack: number[], needle: number): boolean {
           const m = Math.floor(lo + (hi - lo)/2)
           // value to check
           // because of 0 indexing, this will be the midpoint for odd lengths
-          // one after the midpoint for even lengths
+          // one before the midpoint for even lengths
           const v = haystack[m]
           // if the value is the one you're looking for, you're done
           if (v == needle){

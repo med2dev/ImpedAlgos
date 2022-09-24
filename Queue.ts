f9f6e8158e117e7f6504c47c943d483f3921a4b6
hatdeck <daniel.bryce.hall@gmail.com>
2022-09-23 17:19:55 -0700

    implement queue constructor and peek

--- Queue.ts
+++ Queue.ts
@@ -1,18 +1,30 @@
-export default class Queue<T> {
-    public length: number;
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
 
+export default class Queue<T> {
+    public length: number
+    private head?: Node<T>
+    private tail?: Node<T>
 
 
     constructor() {
+      this.head = this.tail = undefined
+      this.length = 0
     }
+
 
     enqueue(item: T): void {
 
-}
+
+    }
+
     deque(): T | undefined {
 
-}
-    peek(): T | undefined {
+    }
 
-}
+    peek(): T | undefined {
+        return this.head?.value
+    }
 }

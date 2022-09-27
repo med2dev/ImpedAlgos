555c4bddb11ea9fb306cb44ef816ab12a5afc53d
hatdeck <daniel.bryce.hall@gmail.com>
2022-09-26 23:57:09 -0700

    stack

---
+++ Stack.ts
@@ -0,0 +1,22 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>
+}
+
+export default class Stack<T>{
+    constructor(){
+
+    }
+
+    push(item: T): void{
+
+    }
+
+    pop(): T | undefined {
+
+    }
+
+    peek(): T | undefined {
+
+    }
+}
